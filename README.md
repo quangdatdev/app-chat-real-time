e264e0252c9c928298307f2a16f4b334d446af3d
QuangDat <quangdat123789@gmail.com>
2024-03-26 17:24:13 +0700

    Setup project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Chatter
+Ứng dụng trò chuyện trực tuyến
---
+++ chatter-api/models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  avatar: String,
+  phone: String,
+  email: String,
+  password: String,
+  contacts: [{type: mongoose.Types.ObjectId, ref: 'User'}],
+  blocked: [{type: mongoose.Types.ObjectId, ref: 'User'}],
+  blockedFrom: [{type: mongoose.Types.ObjectId, ref: 'User'}],
+  createdAt: {type: Date, default: new Date()},
+});
+
+UserSchema.methods.setPassword = function (password) {
+  this.password = bcrypt.hashSync(password, 8);
+};
+
+UserSchema.methods.validatePassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+UserSchema.methods.generateJWT = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+    },
+    "secret"
+  );
+};
+
+UserSchema.methods.toAuthJSON = function () {
+  return {
+    _id: this._id,
+    email: this.email,
+    token: this.generateJWT()
+  };
+};
+
+module.exports = mongoose.model('User', UserSchema);
